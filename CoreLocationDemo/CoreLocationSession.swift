529cb077fb269c43fe0a9b0149ceb1146bfebdb8
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-04-04 14:19:09 -0400

    added CoreLocationSession class

--- CoreLocationDemo/CoreLocationSession.swift
+++ CoreLocationDemo/CoreLocationSession.swift
@@ -8,4 +8,51 @@
 import Foundation
 import CoreLocation
 
+class CoreLocationSession: NSObject {
+    
+    public var locationManager: CLLocationManager
+    
+    override init() { // must mark with override if inheriting from NSObject
+        locationManager = CLLocationManager()
+        super.init()
+        locationManager.delegate = self
+        
+    }
+}
 
+extension CoreLocationSession: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError: \(error)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) { // the state of access for location services
+        switch manager.authorizationStatus {
+        case .authorizedAlways:
+            print("authorizedAlways")
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+        case .denied:
+            print("denied")
+        case .notDetermined:
+            print("notDetermined")
+        case .restricted:
+            print("restricted")
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("didEnterRegion")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("didExitRegion")
+    }
+    
+}
--- CoreLocationDemo/ViewController.swift
+++ CoreLocationDemo/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let locationSession = CoreLocationSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
 
-
 }
-
